54dc4fd4065aa5b90c7ed641f5be242713a31250
Evgeniy Artemov <evgenev2222@gmail.com>
2022-10-04 16:33:58 +0300

    Added brain-even game

--- Makefile
+++ Makefile
@@ -9,3 +9,6 @@ publish:
 
 lint:
 	npx eslint .
+
+fixlint: 
+	npx eslint --fix
---
+++ bin/brain-even.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+/* eslint linebreak-style: ["error", "windows"] */
+
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const askQuestion = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const getRandomNum = Math.floor((Math.random() * 100) - 1);
+    const correctAnswer = getRandomNum % 2 === 0 ? 'yes' : 'no';
+
+    console.log(`Question: ${getRandomNum}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      return (console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`));
+    }
+  }
+  return `Congratulations, ${userName}`;
+};
+
+askQuestion();
