27681d958d91bd04990a8f860f5bed216e0db669
Evgeniy Artemov <evgenev2222@gmail.com>
2022-10-19 15:20:39 +0300

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 import readlineSync from 'readline-sync';
 /* eslint linebreak-style: ["error", "unix"] */
 
+export const countRounds = 3;
+
 const startGame = (description, getAnswerAndQuestion) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 
   console.log(description);
-
-  const countRounds = 3;
 
   for (let i = 0; i < countRounds; i += 1) {
     const [question, correctAnswer] = getAnswerAndQuestion();
